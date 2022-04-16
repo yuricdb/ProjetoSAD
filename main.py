8a90ef8ef113d942bd03cb12af9026f10bef70d4
yuric <ycb@cin.ufpe.br>
2022-04-15 21:04:13 -0300

    Método TOPSIS Finalizado
    Método TOPSIS com todos os cálculos e ranqueamento finalizados, faltando apenas revisar e decidir métodos de entrada do usuário.

--- main.py
+++ main.py
@@ -12,8 +12,6 @@ metTopsisColA = df.iloc[3:18,2:10] #matriz, com a tabela critérios, da coluna A
 metTopsisColASq = np.square(metTopsisColA) #todos os valores da matriz ao quadrado
 metTopsisColASum = metTopsisColASq.sum() #soma de todos valores da matriz por coluna
 
-#normalizacao = np.sqrt(metTopsisColASum) #raiz quadrada de todos os valores por colunas somados
-#print(normalizacao)
 normalizacao = []
 
 for i in metTopsisColASum:
@@ -21,8 +19,68 @@ for i in metTopsisColASum:
 
 PESOS = float(1/8)
 
-print(normalizacao) #debugando valores batem com a planilha
-
+#print(normalizacao) #debugando valores batem com a planilha
 
 #QUADRO 2 - CRITÉRIOS 
+quadro2 = {}
 
+for col in range(metTopsisColA.shape[1]):
+    quadro2[f'coluna{col}'] = []
+    for lin in range(metTopsisColA.shape[0]):
+        quadro2[f'coluna{col}'].append(round((metTopsisColA.iloc[lin,col]/normalizacao[col]*PESOS),4))
+
+idealPosNeg = [[],[]] #criei esse array duplo pra que não fosse preciso criar dois laços pra min() e max() 
+quadro2Dtf = pd.DataFrame(quadro2)
+
+for i in quadro2.keys(): #lista 1 recebe os valores max (ideal positiva) e a lista 2 recebe os minimos (ideal negativa)
+    #o condicional inverte o min e máx de acordo com a legenda de critérios
+    if i == 'coluna5' or i == 'coluna6' or i == 'coluna7':
+        quadro2[i].append(quadro2Dtf[i].min())
+        quadro2[i].append(quadro2Dtf[i].max())
+        #idealPosNeg[0].append(quadro2Dtf[i].min())
+        #idealPosNeg[1].append(quadro2Dtf[i].max())
+    else: 
+        quadro2[i].append(quadro2Dtf[i].max())
+        quadro2[i].append(quadro2Dtf[i].min())
+
+dMaisDMenos = [[],[],[]]
+valorTempDMais = 0
+valorTempDMenos = 0
+
+for i in range(len(quadro2[f'coluna0'])-2): #por questões de arredondamentos os resultados estão próximos, mas não literalmente iguais ao da planilha
+    for j in range(len(quadro2.keys())):
+        valorTempDMais += (quadro2[f'coluna{j}'][i] - quadro2[f'coluna{j}'][-2])**2 #para d+
+        valorTempDMenos += (quadro2[f'coluna{j}'][i] - quadro2[f'coluna{j}'][-1])**2 #para d-
+    valorTempDMais = round((np.sqrt(valorTempDMais)),7)
+    valorTempDMenos = round((np.sqrt(valorTempDMenos)),7)
+    dMaisDMenos[0].append(valorTempDMais)
+    dMaisDMenos[1].append(valorTempDMenos)
+    dMaisDMenos[2].append(round((valorTempDMenos / (valorTempDMenos + valorTempDMais)),6)) #para D
+    valorTempDMais = 0
+    valorTempDMenos = 0
+
+#add a coluna d+, d- e D à tabela
+quadro2['coluna8'] = dMaisDMenos[0] 
+quadro2['coluna9'] = dMaisDMenos[1]
+quadro2['coluna10'] = dMaisDMenos[2]
+
+#Ranqueamento TOPSIS
+#deixando a tabela com mesmo número de linhas pra cada coluna (pra que se possa transformar em dataframe)
+for i in quadro2.keys():
+    if not(i== 'coluna8' or i == 'coluna9' or i == 'coluna10'):
+        quadro2[i].pop()
+        quadro2[i].pop()
+
+quadro2Dtf = pd.DataFrame(quadro2)
+
+#ordenação:
+ranqTopsis = quadro2Dtf.sort_values(by = 'coluna10', ascending= False)
+ranqTopsisInvertido = quadro2Dtf.sort_values(by = 'coluna10') #ranqueado, porém tá de trás pra frente
+
+#debugar mostrar tabela ordenada
+print(ranqTopsis) 
+print(ranqTopsisInvertido)
+
+#legenda da planilha:
+#coluna 0 até 7 (colunas de A até H), colunas 8 até 10 (d+, d-, D)
+#nomes dos projetos não foram colocados ainda pq devemos decidir como ficará nos inputos de usuários
